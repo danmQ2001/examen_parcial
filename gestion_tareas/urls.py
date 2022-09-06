4b617c10eba1d72e5d68df435e7caba872f71c2e
Dany <xdany2001x@hotmail.com>
2022-09-05 23:29:35 -0500

    'V7'

--- gestion_tareas/urls.py
+++ gestion_tareas/urls.py
@@ -5,8 +5,8 @@ app_name='gestion_tareas'
 
 urlpatterns=[
     path('',views.login,name='login'),
-    path('dashboard',views.dashboard,name='dashboard'),
-    path('crear_tarea',views.crear_tarea,name='crear_tarea'),
+    path('dashboard/<str:id_user>',views.dashboard,name='dashboard'),
+    path('crear_tarea/<str:mantener_id>',views.crear_tarea,name='crear_tarea'),
     path('editar_tarea/<str:ind>',views.editar_tarea,name='editar_tarea'),
 ]
 
--- gestion_tareas/views.py
+++ gestion_tareas/views.py
@@ -16,29 +16,30 @@ def login(request):
         for persona in usuarios_totales:
             if persona.nombre == nombreUsuario and persona.password == passwordUsuario:
                 usuario_registrado=1
+                id_person=persona.id
        
         if usuario_registrado==1:
-            return HttpResponseRedirect(reverse('gestion_tareas:dashboard'),{
-                'usuario_ingresado': nombreUsuario,
-            })
+            return HttpResponseRedirect(reverse('gestion_tareas:dashboard', kwargs={'id_user' : id_person})
+            )
         else:
             return render(request,'gestion_tareas/login.html',{
                 'mensaje':'Los datos ingresados no son correcto o no están registrados',
             })            
     return render(request,'gestion_tareas/login.html')
-def dashboard(request):
+def dashboard(request,id_user):
     tareas_totales = tarea.objects.all()
     #Filtrar tareas del usuario
     lista_tareas = []
-
-    tareas_totales = tarea.objects.filter(usuario_responsable='dany')
+    user_responsable=usuario.objects.get(id=id_user)
+    tareas_totales = tarea.objects.filter(usuario_responsable= user_responsable.nombre)
     for homework in tareas_totales:   
         lista_tareas.append(homework)
     #ciclo de filtración finalizado
     return render(request,'gestion_tareas/dashboard.html',{
         'objTarea':lista_tareas,
+        'user':id_user,
     })
-def crear_tarea(request):
+def crear_tarea(request,mantener_id):
     if request.method =='POST':
         descripcion = request.POST.get('descripcion')
         fecha_creacion=request.POST.get('fecha_creacion')
@@ -47,18 +48,23 @@ def crear_tarea(request):
         fecha_entrega=parse(fecha_entrega)
         usuario_responsable=request.POST.get('usuario_responsable')
         tarea(descripcion=descripcion,fecha_creacion=fecha_creacion,fecha_entrega=fecha_entrega,usuario_responsable=usuario_responsable,estado_tarea='').save()
-        return HttpResponseRedirect(reverse('gestion_tareas:dashboard'))
-    return render(request,'gestion_tareas/crear_tarea.html')
+        return HttpResponseRedirect(reverse('gestion_tareas:dashboard',kwargs={'id_user' : mantener_id}))
+    return render(request,'gestion_tareas/crear_tarea.html',{
+        'id_persona':mantener_id,
+    })
 
 def editar_tarea(request,ind):
     EditarTarea=tarea.objects.get(id=ind)
+    persona=usuario.objects.get(nombre=EditarTarea.usuario_responsable)
+    id_persona=persona.id
     if request.method=='POST':
         EditarTarea.descripcion = request.POST.get('descripcion')
         EditarTarea.fecha_creacion=request.POST.get('fecha_creacion')
         EditarTarea.fecha_entrega=request.POST.get('fecha_entrega')
         EditarTarea.usuario_responsable=request.POST.get('usuario_responsable')
         EditarTarea.save()
-        return HttpResponseRedirect(reverse('gestion_tareas:dashboard'))
+        return HttpResponseRedirect(reverse('gestion_tareas:dashboard',kwargs={'id_user' : id_persona}))
     return render(request,'gestion_tareas/editar_tarea.html',{
         'tarea_info':EditarTarea,
+        'id_username':id_persona,
     })    
