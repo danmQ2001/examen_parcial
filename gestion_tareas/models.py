21b1219bfb80c24ea5df0976628cb66a675e6f78
Dany <xdany2001x@hotmail.com>
2022-09-09 02:15:43 -0500

    'V11'

--- gestion_tareas/models.py
+++ gestion_tareas/models.py
@@ -1,10 +1,11 @@
+from datetime import date, datetime
 from django.db import models
 
 # Create your models here.
 class tarea(models.Model):
     descripcion = models.CharField(max_length=128,default='')
-    fecha_creacion= models.CharField(max_length=128,default='')
-    fecha_entrega= models.CharField(max_length=128,default='')
+    fecha_creacion= models.DateField(default=date.today())
+    fecha_entrega= models.DateField(default=date.today())
     usuario_responsable= models.CharField(max_length=128,default='')
     estado_tarea= models.CharField(max_length=128,default='')
 
--- gestion_tareas/views.py
+++ gestion_tareas/views.py
@@ -3,7 +3,7 @@ from gestion_tareas.models import usuario,tarea
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from dateutil.parser import parse
-from datetime import date, time, datetime
+from datetime import date, datetime
 # Create your views here.
 def login(request):
     if request.method=='POST':
@@ -34,25 +34,31 @@ def dashboard(request,id_user,tarea_id_finalizar):
     user_responsable=usuario.objects.get(id=id_user)
     tareas_totales = tarea.objects.filter(usuario_responsable= user_responsable.nombre)
     if tarea_id_finalizar != '0' and tarea_id_finalizar != 0:
-        finalizar_tarea=tarea.objects.get(id=tarea_id_finalizar)
+        finalizar_tarea=tarea.objects.get(id=int(tarea_id_finalizar))
     for homework in tareas_totales: 
-        if tarea_id_finalizar != '0'and tarea_id_finalizar != 0 :
+        if tarea_id_finalizar != '0' and tarea_id_finalizar != 0 :
             if homework.id==finalizar_tarea.id:
                 homework.estado_tarea='3'
                 homework.save()
     # Calcular cantidad de días para realizar la tarea
-        fecha_f=datetime.strptime(str(homework.fecha_entrega), "%Y-%m-%d")
-        fecha_i=datetime.today()
-        remaining_days = (fecha_f - fecha_i).days 
-        if remaining_days>2 and homework.estado_tarea !='3':
-            homework.estado_tarea='1'
-        elif remaining_days<2 and remaining_days>=0 and homework.estado_tarea !='3':
-            homework.estado_tarea='2'
-        elif homework.estado_tarea !='3':
-            homework.estado_tarea='4'
+        ga=homework.fecha_entrega
+
+
+        fecha_f=homework.fecha_entrega
+        fecha_i=date.today()
+
+
+        remaining_days = (fecha_f - fecha_i).days
+        if homework.estado_tarea !='3':
+            if remaining_days>2 :
+                homework.estado_tarea='1'
+            elif remaining_days<=2 and remaining_days>=0 :
+                homework.estado_tarea='2'
+            else :
+                homework.estado_tarea='4'
     #ciclo de filtración finalizado
         lista_tareas.append(homework)
-        tarea_id_finalizar=0
+        #tarea_id_finalizar=0
     return render(request,'gestion_tareas/dashboard.html',{
         'objTarea':lista_tareas,
         'user':id_user,
@@ -78,11 +84,11 @@ def editar_tarea(request,ind):
     id_persona=persona.id
     if request.method=='POST':
         EditarTarea.descripcion = request.POST.get('descripcion')
-        EditarTarea.fecha_creacion=request.POST.get('fecha_creacion')
+        EditarTarea.fecha_creacion=request.POST.get('fecha_creacion')    
         EditarTarea.fecha_entrega=request.POST.get('fecha_entrega')
         EditarTarea.usuario_responsable=request.POST.get('usuario_responsable')
         EditarTarea.save()
-        return HttpResponseRedirect(reverse('gestion_tareas:dashboard',kwargs={'id_user' : id_persona}))
+        return HttpResponseRedirect(reverse('gestion_tareas:dashboard',kwargs={'id_user' : id_persona, 'tarea_id_finalizar' : 0}))
     return render(request,'gestion_tareas/editar_tarea.html',{
         'tarea_info':EditarTarea,
         'id_username':id_persona,
